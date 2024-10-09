e9524ff7df76ac3b98077d80deb8d51956cdb618
ItsMeMahiG <mahi.gadicherlaboba@gmail.com>
2024-10-09 12:02:33 +0530

    python operators 2

---
+++ and or opterator.py
@@ -0,0 +1,8 @@
+a=24
+b=25
+c=23
+if a>b or a<c :
+    print("I am using 'or' operator here")
+
+elif a<b and a>c :
+    print("I am using 'and' operator here")
---
+++ not operator.py
@@ -0,0 +1,19 @@
+#activity 2
+a=12
+b=123
+c=123
+print(a!=b)
+print(b!=c)
+
+#activity 3
+a="coconut"
+b="coconut water"
+if a!=b :
+    print(a," and",b, " are not similar")
+
+#activity 3
+a=12
+b=13
+if (a==b) != (b==13) :
+    print("hi sup")
+    
