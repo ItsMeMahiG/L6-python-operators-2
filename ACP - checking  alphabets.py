6a017f9bd41420a6a2ad3f148617fc74eb5f7c93
ItsMeMahiG <mahi.gadicherlaboba@gmail.com>
2024-10-10 11:24:25 +0530

    opterators with acp

---
+++ ACP - checking  alphabets.py
@@ -0,0 +1,7 @@
+ch=input("enter a character")
+if ch >='a' and ch <= 'z' :
+    print("the character is an alphabet")
+elif ch >='A' and ch<='Z' :
+    print("the character is an alphabet")
+else :
+    print("the character is not an alphabet")
